53aedb18c03da9e56f353cd6f9c3251ab8a9a7f3
Ankur Baranwal <ankur.baranwal@appinventiv.com>
2025-10-05 18:42:55 +0530

    Remove unwanted code and file converted

--- Sources/LokaliseKit/Localization.swift
+++ Sources/LokaliseKit/Localization.swift
@@ -6,23 +6,13 @@ import Foundation
 
 public extension String {
     
-    public enum DefaultLocalization {
-        case unknown
-        
-        public var key: String {
-            switch self {
-            case .unknown:
-                return ""
-            }
-        }
-    }
-    
-    public func fetchTranslationFromLocalBundle() -> String {
+    internal func fetchTranslationFromLocalBundle() -> String {
         guard !(LocalizableBundleManager.shared.isLocalLocalizableBundleIsLoaded())  else {
             return "" }
         let translation = NSLocalizedString(self, tableName: "", bundle: LocalizableBundleManager.shared.getLocalBundle(), value: "", comment: "")
         return translation == self ? "" : translation
     }
+    
     public func localized() -> String {
 
         let initialTranslation = NSLocalizedString(self, tableName: "", bundle: LocalizableBundleManager.shared.currentBundle, value: "", comment: "")
